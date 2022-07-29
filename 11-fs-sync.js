0309c05117ac6863b6c0ae7204f51b560b561c85
Adarsh Chandran <adarsh.chandran@joingsg.com>
2022-07-29 17:54:59 +0200

    Add writeToFile function in fs-sync

--- 11-fs-sync.js
+++ 11-fs-sync.js
@@ -1,19 +1,14 @@
 //--Fs -sync
 
-// const{readFileSync} = require('fs');
-        //or
+const fs = require('fs')
 
-const { Console } = require('console');
-const fs =  require('fs')
-//fs.readFileSync();
-// fs.writeFileSync();
-
-const first = fs.readFileSync('./content/first.txt','utf-8');
-const second = fs.readFileSync('./content/second.txt','utf-8');
-
-// console.log(first);
-// console.log(second);
-
-
-const wirteFile = fs.writeFileSync('./content/result-sync.txt',`This is the result:${first}, ${second}`);
-console.log(wirteFile);
+const writeToFile = async (data, path) => {
+  const jsonString = JSON.stringify(data)
+  fs.writeFileSync(path, jsonString, err => {
+    if (err) {
+      console.log('Error writing file', err)
+    } else {
+      console.log('Successfully wrote file')
+    }
+  })
+}
